23cebab80c3cb82c23fb4ad795988e706c441ae9
Dennis Bae <dennisbae7@gmail.com>
2021-01-17 23:50:12 -0500

    second lessson

--- Functions-at-Runtime/callback.js
+++ Functions-at-Runtime/callback.js
@@ -9,4 +9,13 @@ function returnThree() {
 }
 callAndAdd(2, returnThree) // 5
 
+// Callback functions are used in array methods
+
+// forEach() takes in callback function and invokes the function for each element in array
+// This is similar to for loop, looping through array
+[1, 5, 2, 4, 6, 3].forEach(function logIfOdd(n) {
+    if (n % 2 !== 0) {
+      console.log(n);
+    }
+});
 
